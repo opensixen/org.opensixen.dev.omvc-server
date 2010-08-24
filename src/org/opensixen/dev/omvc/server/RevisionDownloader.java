4ee2e25948dbbb15a5a3bd20e14a4e6b34d9a875
Eloy Gomez <eloy@indeos.es>
2010-08-24 16:04:15 +0000

    Añadimos fecha a los datos de la revision Solucionamos bug por el que no se descargaban las actualizaciones en el orden correcto

--- src/org/opensixen/dev/omvc/server/RevisionDownloader.java
+++ src/org/opensixen/dev/omvc/server/RevisionDownloader.java
@@ -3,6 +3,7 @@ package org.opensixen.dev.omvc.server;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.opensixen.dev.omvc.interfaces.IRevisionDownloader;
 import org.opensixen.dev.omvc.model.Project;
@@ -23,6 +24,7 @@ public class RevisionDownloader extends AbstractRienaServer implements IRevision
 		Criteria crit = HSession.getCriteria(Revision.class);
 		crit.add(Restrictions.eq("project", project));
 		crit.add(Restrictions.gt("revision_ID", from));
+		crit.addOrder(Order.asc("revision_ID"));
 		return crit.list();
 	}
 
@@ -31,6 +33,7 @@ public class RevisionDownloader extends AbstractRienaServer implements IRevision
 		Criteria crit = HSession.getCriteria(Script.class);
 		crit.add(Restrictions.eq("revision", revision));
 		crit.add(Restrictions.in("engine", engines));
+		crit.addOrder(Order.asc("script_ID"));
 		return crit.list();
 	}
 
